c0b896d1b88d4d4f47532dcf659ff3b8968723a2
Malaka Mahanama <malaka.open.source@gmail.com>
2015-08-01 19:32:48 -0500

    adding error handling class and v7 jar

--- src/com/lanka/manapa/utils/ErrorHandling.java
+++ src/com/lanka/manapa/utils/ErrorHandling.java
@@ -1,7 +1,7 @@
 package com.lanka.manapa.utils;
 
 public class ErrorHandling {
-	
+	//centralize error handling. - not yet in use
 	String SMS_COUNT_REQUIRED = "Please enter a number";
 	String SMS_BODY_REQUIRED = "Please enter the message";
 	String SMS_RECIEVER_NUMBER_REQUIRED = "Please add a telephone number";
